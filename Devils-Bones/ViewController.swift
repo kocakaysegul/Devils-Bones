7ffd58cfc9e6ec95036ab58c6442d018dbad58e4
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-10 13:27:50 +0300

    dice images randomised

--- Devils-Bones/ViewController.swift
+++ Devils-Bones/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFive")
+    
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),]
+        
+        diceImageView1.image = diceArray[Int.random(in: 1...5)]
+        diceImageView2.image = diceArray.randomElement()
+    }
+    
 }
 
