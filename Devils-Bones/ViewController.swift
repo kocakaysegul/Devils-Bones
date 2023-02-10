f7458cc581939104897a99ad90337bd8dfe6e082
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-10 12:20:31 +0300

    linked design to code

--- Devils-Bones/ViewController.swift
+++ Devils-Bones/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFive")
     }
 
 
